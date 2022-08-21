68d76f01ea8f243f6027f4cfd0185f32e2b7e552
farhdibehnamdev <97886945+farhdibehnamdev@users.noreply.github.com>
2022-08-21 23:30:06 +0430

    reactor:Refactored Route to update our route urls.

--- src/router/route.js
+++ src/router/route.js
@@ -1,4 +1,25 @@
+import { useEffect, useReducer } from "react";
+import { unregister, register } from "./register";
+
+const generateId = (function () {
+  let id = 0;
+  return () => {
+    id += 1;
+    return id;
+  };
+})();
+
 export const Route = function ({ path, children }) {
+  const [ignore, forceUpdate] = useReducer((x) => x + 1, 0);
+
+  useEffect(() => {
+    const id = generateId();
+    register(id, forceUpdate);
+    return () => {
+      unregister(id);
+    };
+  }, []);
+
   const { pathname } = window.location;
   if (pathname !== path) return null;
   return children;
